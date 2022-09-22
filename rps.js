d67404515117dd946eafa7585b39434be9c52eff
Jenny Dixon <jennyrmdixon@gmail.com>
2022-09-21 21:11:47 -0700

    Add UI

--- rps.js
+++ rps.js
@@ -1,80 +1,79 @@
-//Allows user to enter their guess and display it back to them
-function playerTurn() {
-    let playerSelection = prompt("Enter Your Choice.", "").toLowerCase();
-    console.log("You guessed: " + playerSelection);
-    window.playerSelection = playerSelection;
-}
+ let userScore = 0;
+ let computerScore = 0;
+ let roundMessage = "Click a button to start a round.";
+ let gameMessage = "Results of game will be displayed once a player gains 5 points.";
 
-//Generates choice for computers
-function computerChoice() {
-    let computerOptions = ["rock", "paper", "scissors"]
-    return computerOptions[Math.floor(Math.random() * 3)];
-}
+ //Play round and track results up until 5 points are earned
 
-//Computer places guess, returns response, invokes round
-function computerTurn() {
-    let computerSelection = computerChoice();
-    console.log("Computer guessed: " + computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-}
+function playRound(playerSelection) {
 
-//Used to keep score at each round
-let scoreCounter = 0;
+    //Computer makes a random selection.
+    let computerOptions = ["rock", "paper", "scissors"];
+    let computerSelection = computerOptions[Math.floor(Math.random() * 3)];
 
-
-//Determine winner of round
-function playRound(playerSelection, computerSelection) {
+    //Determine results of the round
     if (playerSelection === computerSelection) {
-        return "It's a tie";
+        roundMessage = "It's a tie";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        scoreCounter = ++scoreCounter;
-        return "You win! Rock beats scissors.";
+        userScore = ++userScore;
+        roundMessage = "You win! Rock beats scissors.";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        scoreCounter = ++scoreCounter;
-        return "You win! Paper beats rock.";
+        userScore = ++userScore;
+        roundMessage = "You win! Paper beats rock.";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        scoreCounter = ++scoreCounter;
-        return "You win! Scissors beats paper.";
+        userScore = ++userScore;
+        roundMessage = "You win! Scissors beats paper.";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        scoreCounter = --scoreCounter;
-        return "You lose! Paper beats rock.";
+        computerScore = ++computerScore;
+        roundMessage = "You lose! Paper beats rock.";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        scoreCounter = --scoreCounter;
-        return "You lose! Rock beats scissors.";
+        computerScore = ++computerScore;
+        roundMessage =  "You lose! Rock beats scissors.";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        scoreCounter = --scoreCounter;
-        return "You lose! Scissors beats paper.";
+        computerScore = ++computerScore;
+        roundMessage = "You lose! Scissors beats paper.";
     } else {
-        return "I don't understand your answer.";
+        roundMessage = "I don't understand your answer.";
 
     }
-}
+    //Display results of the round
+    displayRoundResults.textContent = roundMessage;
 
-//Play a five round game and return results
-function game() {
-    for (let i = 0; i < 5; i++) {
-        if (i < 4) {
-            playerTurn();
-            computerTurn();
-        }
-        else if (i = 4) {
-            playerTurn();
-            computerTurn();
-            if (scoreCounter === 0) {
-                console.log("The game ended in a tie.");
-            }
-            else if (scoreCounter > 0) {
-                console.log("You won the game!");
-            }
-            else if (scoreCounter < 0) {
-                console.log("You lost the game!");
-            }
-            else {98 cs=8
-                console.log("Something went wrong.");
-
-            }
-        }
+    //Determine results of the round
+    if (computerScore === 5) {
+        gameMessage = "You lost the game";
     }
-}
-game();
+    else if (userScore === 5) {
+        gameMessage = "You won the game!";
+    }
 
+    //Determine results of the game
+    displayGameResults.textContent = gameMessage;
+
+}
+
+//Take player choice from button
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+let playerSelection;
+
+rockButton.addEventListener('click', () => {
+playRound("rock");
+  });
+
+paperButton.addEventListener('click', () => {
+ playRound("paper");
+  });
+  
+scissorsButton.addEventListener('click', () => {
+  playRound("scissors");
+  });
+  
+//Display results to user
+const displayRoundResults = document.querySelector('#roundResults');
+displayRoundResults.textContent = roundMessage;
+const displayGameResults = document.querySelector('#gameResults');
+
+  
